e658102b373e3518f2db6d82a23c1d20bec490ab
Rajgokul R M <rajgokul.r@jmangroup.com>
2025-02-05 12:33:24 +0530

    made responsive pages

--- src/pages/BlogDetail.jsx
+++ src/pages/BlogDetail.jsx
@@ -30,13 +30,13 @@ const BlogDetail = () => {
   }
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-[25%,50%,25%] ">
+    <div className="grid grid-cols-1 md:grid-cols-[1fr,2fr,1fr] gap-4 p-4 md:p-8">
       <div className="hidden md:block"></div> 
   
       <div className="">
         <img src={blog.image} alt={blog.title} className="w-full h-100 object-cover rounded mb-4" />
         <h2 className="text-2xl font-bold mb-2 text-center">{blog.title}</h2>
-        <p className="text-gray-700 text-justify leading-relaxed">{blog.content}</p>
+        <p className="text-gray-700 text-justify leading-relaxed whitespace-pre-line">{blog.content}</p>
       </div>
   
       <div className="hidden md:block"></div> 
--- src/pages/HomeContent.jsx
+++ src/pages/HomeContent.jsx
@@ -62,7 +62,7 @@ const HomeContent = () => {
   };
 
   return (
-    <div className="grid grid-cols-3 gap-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
       {blogs.map((blog, index) => (
         <div key={index} className="border p-4">
           <Link to={`/blog/${index}`} className="block" onClick={() => handleView(index)}>
